65620dc37d0e24a330dafb8e7fead115a135c7da
Alexsander Uglin <uglin76@mail.ru>
2021-05-12 16:23:02 +0300

    refactoring

--- habr_parser.py
+++ habr_parser.py
@@ -10,19 +10,19 @@ class HabrParser:
         self.browser = mechanicalsoup.StatefulBrowser()
         self.url = url
         self.search_pattern = '|'.join([f"\\b{word}\\b" for word in keywords])        
-        self.articles = self.__get_articles()                
+        self.articles = self._get_articles()
 
-    def __get_articles(self):
+    def _get_articles(self):
         self.browser.open(self.url)
         return self.browser.get_current_page().find_all('article', class_='post_preview')
 
     @staticmethod
-    def __date_beautify(date):
+    def _date_beautify(date):
         today = str(dt.date(dt.now()))
         yesterday = str(dt.date(dt.now()) - timedelta(days=1))
         return date.replace('сегодня', today).replace('вчера', yesterday)
 
-    def __get_article_full_text(self, link):
+    def _get_article_full_text(self, link):
         self.browser.open(link)
         article_text = self.browser.get_current_page().find('div', class_='post__body')
         return article_text.text if article_text else ''
@@ -30,11 +30,11 @@ class HabrParser:
     def find_articles_by_keywords(self):
         find_in_preview, find_in_full_text = (), ()
         for article in tqdm(self.articles, desc='collect data:'):
-            date = self.__date_beautify(article.find('span', class_='post__time').text)
+            date = self._date_beautify(article.find('span', class_='post__time').text)
             link = article.find('a', class_='post__title_link').get('href')
             title = article.find('a', class_='post__title_link').text
             preview = article.find('div', class_='post__text').text
-            full_text = self.__get_article_full_text(link)
+            full_text = self._get_article_full_text(link)
 
             if re.search(self.search_pattern, preview, flags=re.I):
                 found_words = re.findall(self.search_pattern, preview, flags=re.I)
